67cc7c0f45fc3f5f994bc86771d6051a27648565
Vadim <mdiv551@gmail.com>
2021-08-13 19:08:43 +0300

    Add-new-route
    Add new route for restore user state

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -5,6 +5,8 @@ const isGuest = require('../../extensions/middlewares/isGuest');
 const isUser = require('../../extensions/middlewares/isUser');
 
 router.all('/signIn', isGuest, require('./signIn'));
+//If user has cookie connect.sid and this is cookie is correctly, return user data.
+router.post('/restoreState', isUser, require('./restoreState'));
 router.post('/signUp', isGuest, require('./signUp'));
 router.get('/signOut', isUser, require('./signOut'));
 
