352eae2e6906db2babbe9fef366703b54c936c5f
Vadim <mdiv551@gmail.com>
2021-08-24 20:34:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const expressWs = require('express-ws')(app);
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
-const port = 3000;
+const port = process.env.PORT || 80;
 
 const { Passport } = require('./extensions/passport');
 const { Settings } = require('./settings/Settings');
